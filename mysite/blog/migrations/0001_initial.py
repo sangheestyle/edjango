e0d4737d15fe70344923352bfb6dd3eb2b1a8ee3
Sanghee Kim <sangheestyle@gmail.com>
2016-01-31 13:27:06 -0500

    add migration file for blog
    $ python manage.py makemigrations blog
    
    Then see what is inside of the file
    
    $ python manage.py sqlmigrate blog 001
    
    Then apply it into DB
    
    $ python manage.py migrate
    
    Done.

---
+++ mysite/blog/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=250)),
+                ('slug', models.SlugField(max_length=250, unique_for_date='publish')),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=datetime.datetime(2016, 1, 31, 18, 20, 29, 141251, tzinfo=utc))),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(max_length=10, default='draft', choices=[('draft', 'Draft'), ('published', 'Published')])),
+                ('author', models.ForeignKey(related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]
